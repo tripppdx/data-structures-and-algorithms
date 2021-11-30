6fe0af6478cb4333297f4b3e87d82c9fc3b56525
Tripp Lucas <tripppdx@gmail.com>
2021-11-29 23:35:28 -0800

    update readme

--- javascript/code-challenges/quick-sort/quick-sort.js
+++ javascript/code-challenges/quick-sort/quick-sort.js
@@ -1,16 +1,20 @@
 'use strict';
+let x = 0;
 
 function quickSort(arr, left, right) {
   if (left < right) {
     let position = partition(arr, left, right);
+    console.log('position', position);
     quickSort(arr, left, position - 1);
     quickSort(arr, position + 1, right);
   }
+  x++;
   return arr;
 }
 
 function partition(arr, left, right) {
   let pivot = arr[right];
+  console.log('pivot', pivot);
   let low = left - 1;
   for (let i = left; i < right; i++) {
     if (arr[i] <= pivot) {
@@ -23,6 +27,7 @@ function partition(arr, left, right) {
 }
 
 function swap(arr, i, low) {
+  console.log(arr);
   let temp = arr[i];
   arr[i] = arr[low];
   arr[low] = temp;
@@ -30,5 +35,6 @@ function swap(arr, i, low) {
 
 let arr = [8, 4, 23, 42, 16, 15];
 console.log(quickSort(arr, 0, arr.length - 1));
+console.log('x', x);
 
 module.exports = quickSort;
